53c56218b0f496c564c0c2fd83fd1bbae3ef9093
Timothy Lentz <tlentz@digicomlearning.com>
2018-08-01 13:11:38 -0700

    Crud ready

--- src/features/event/EventList/EventListItem.jsx
+++ src/features/event/EventList/EventListItem.jsx
@@ -5,7 +5,7 @@ import EventListAtt from './EventListAtt'
 class EventListItem extends Component {
     render() {
 
-        const {event} = this.props;
+        const { event, onEventOpen, deleteEvent } = this.props;
 
         return (
             <Segment.Group>
@@ -33,12 +33,13 @@ class EventListItem extends Component {
                         {event.attendees && event.attendees.map((attendee) => (
                             <EventListAtt key={attendee.id} attendee={attendee} />
                         ))}
-                        
+
                     </List>
                 </Segment>
                 <Segment clearing>
                     <span>{event.description}</span>
-                    <Button as="a" color="teal" floated="right" content="View" />
+                    <Button onClick={onEventOpen(event)} as="a" color="teal" floated="right" content="View" />
+                    <Button onClick={deleteEvent(event.id)} as="a" color="red" floated="right" content="Delete" />
                 </Segment>
             </Segment.Group>
         )
