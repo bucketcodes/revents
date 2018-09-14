160a9e29b849051b2f7130626095561ab6ed5fc1
Timothy Lentz <tlentz@digicomlearning.com>
2018-09-13 19:15:54 -0700

    End

--- src/features/user/UserDetailed/UserDetailedPage.jsx
+++ src/features/user/UserDetailed/UserDetailedPage.jsx
@@ -1,23 +1,47 @@
 import React, {Component} from 'react';
 import {Button, Card, Grid, Header, Icon, Image, Item, List, Menu, Segment} from "semantic-ui-react";
+import { connect } from 'react-redux'
+import { format } from 'date-fns'
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from 'redux'
+import differenceInYears from 'date-fns/difference_in_years'
+import { UserDetailedEvents } from './UserDetailedEvents'
+
+const query = ({ auth }) => {
+  return [
+    {
+      collection: "users",
+      doc: auth.uid,
+      subcollections: [{ collection: "photos" }],
+      storeAs: "photos"
+    }
+  ];
+};
+
+const mapState = state => ({
+  auth: state.firebase.auth,
+  profile: state.firebase.profile,
+  photos: state.firestore.ordered.photos,
+})
 
 class UserDetailedPage extends Component {
 
     render() {
-
+    const { auth, profile, photos } = this.props;
         return (
             <Grid>
                 <Grid.Column width={16}>
                     <Segment>
                         <Item.Group>
                             <Item>
-                                <Item.Image avatar size='small' src='https://randomuser.me/api/portraits/men/20.jpg'/>
+                                <Item.Image avatar size='small' src={profile.photoURL || '/assets/user.png'}/>
                                 <Item.Content verticalAlign='bottom'>
-                                    <Header as='h1'>First Name</Header>
+                                    <Header as='h1'>{auth.displayName}</Header>
                                     <br/>
-                                    <Header as='h3'>Occupation</Header>
+                                    {profile.occupation && 
+                                    <Header as='h3'>{profile.occupation}</Header>}
                                     <br/>
-                                    <Header as='h3'>27, Lives in London, UK</Header>
+                                    <Header as='h3'>{ profile.dateOfBirth && differenceInYears(Date.now(), profile.dateOfBirth.toDate()) }{profile.city && `, Lives in ${profile.city}` || ', Might be homeless'}</Header>
                                 </Item.Content>
                             </Item>
                         </Item.Group>
@@ -28,29 +52,23 @@ class UserDetailedPage extends Component {
                     <Segment>
                         <Grid columns={2}>
                             <Grid.Column width={10}>
-                                <Header icon='smile' content='About Display Name'/>
-                                <p>I am a: <strong>Occupation Placeholder</strong></p>
-                                <p>Originally from <strong>United Kingdom</strong></p>
-                                <p>Member Since: <strong>28th March 2018</strong></p>
-                                <p>Description of user</p>
+                                <Header icon='smile' content={`About ${auth.displayName}`}/>
+                                {profile.occupation && <p>I am a: <strong>{profile.occupation}</strong></p>}
+                                {profile.origin && <p>Originally from <strong>{profile.origin}</strong></p>}
+                                <p>Member Since: <strong>{profile.createdAt && format(profile.createdAt.toDate(), 'Do MMMM YYYY')}</strong></p>
+                                <p>{profile.about || 'You will just have to ask to find out, won\'t you.'}</p>
 
                             </Grid.Column>
                             <Grid.Column width={6}>
 
                                 <Header icon='heart outline' content='Interests'/>
                                 <List>
+                                  {profile.interests && profile.interests.length >= 1 ? profile.interests.map(interest => (
                                     <Item>
-                                        <Icon name='heart'/>
-                                        <Item.Content>Interest 1</Item.Content>
+                                      <Icon name='heart'/>
+                                      <Item.Content>{interest}</Item.Content>
                                     </Item>
-                                    <Item>
-                                        <Icon name='heart'/>
-                                        <Item.Content>Interest 2</Item.Content>
-                                    </Item>
-                                    <Item>
-                                        <Icon name='heart'/>
-                                        <Item.Content>Interest 3</Item.Content>
-                                    </Item>
+                                  )) : 'No interests!'}
                                 </List>
                             </Grid.Column>
                         </Grid>
@@ -63,62 +81,25 @@ class UserDetailedPage extends Component {
                     </Segment>
                 </Grid.Column>
 
-                <Grid.Column width={12}>
+                {photos && photos.length > 1 && <Grid.Column width={12}>
                     <Segment attached>
                         <Header icon='image' content='Photos'/>
                         
                         <Image.Group size='small'>
-                            <Image src='https://randomuser.me/api/portraits/men/20.jpg'/>
-                            <Image src='https://randomuser.me/api/portraits/men/20.jpg'/>
-                            <Image src='https://randomuser.me/api/portraits/men/20.jpg'/>
-                            <Image src='https://randomuser.me/api/portraits/men/20.jpg'/>
+                            {photos.map(photo => <Image src={photo.url}/>)}
                         </Image.Group>
                     </Segment>
-                </Grid.Column>
-                /* static for now */
-                <Grid.Column width={12}>
-                    <Segment attached>
-                        <Header icon='calendar' content='Events'/>
-                        <Menu secondary pointing>
-                            <Menu.Item name='All Events' active/>
-                            <Menu.Item name='Past Events'/>
-                            <Menu.Item name='Future Events'/>
-                            <Menu.Item name='Events Hosted'/>
-                        </Menu>
+                </Grid.Column>}
 
-                        <Card.Group itemsPerRow={5}>
-
-                            <Card>
-                                <Image src={'/assets/categoryImages/drinks.jpg'}/>
-                                <Card.Content>
-                                    <Card.Header textAlign='center'>
-                                        Event Title
-                                    </Card.Header>
-                                    <Card.Meta textAlign='center'>
-                                        28th March 2018 at 10:00 PM
-                                    </Card.Meta>
-                                </Card.Content>
-                            </Card>
-
-                            <Card>
-                                <Image src={'/assets/categoryImages/drinks.jpg'}/>
-                                <Card.Content>
-                                    <Card.Header textAlign='center'>
-                                        Event Title
-                                    </Card.Header>
-                                    <Card.Meta textAlign='center'>
-                                        28th March 2018 at 10:00 PM
-                                    </Card.Meta>
-                                </Card.Content>
-                            </Card>
-
-                        </Card.Group>
-                    </Segment>
-                </Grid.Column>
+                {/* static for now */}
+                <UserDetailedEvents />
             </Grid>
 
         );
     }
 }
 
-export default UserDetailedPage;
+export default compose(
+  connect(mapState, null),
+  firestoreConnect(auth => query(auth))
+)(UserDetailedPage)
